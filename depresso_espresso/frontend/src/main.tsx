592e939af7b7a021050ac09a3450fd15ae41a8a1
Thea Nguyen <thea25060@gmail.com>
2024-02-10 16:46:07 -0700

    Integrated static files generated from React to Django

--- depresso_espresso/frontend/src/main.tsx
+++ depresso_espresso/frontend/src/main.tsx
@@ -1,14 +1,25 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import App from "./App";
+import Login from "./components/Login.tsx";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const rootElement = document.getElementById("root") as Element;
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<App />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- depresso_espresso/frontend/vite.config.ts
+++ depresso_espresso/frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: () => {
+          return `static/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "static/[name]-[hash].js",
+        entryFileNames: "static/[name]-[hash].js",
+      },
+    },
+  },
+});
