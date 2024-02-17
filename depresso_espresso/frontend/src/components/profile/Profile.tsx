ae9ea3675af160b329e6b508be9e4febdf8364aa
Thea Nguyen <thea25060@gmail.com>
2024-02-17 15:52:45 -0700

    Added extracting the input value from users

--- depresso_espresso/frontend/src/components/profile/Profile.tsx
+++ depresso_espresso/frontend/src/components/profile/Profile.tsx
@@ -1,5 +1,9 @@
 //#region Imports
 import { Button } from "../Button";
+import Popup from "reactjs-popup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
 //#endregion
 
 //#region interfaces
@@ -25,8 +29,54 @@ const Profile = ({
   github,
   avatarURL,
 }: ProfileProps): JSX.Element => {
+  const editFields = [
+    { label: "Display Name", value: "username", placeholder: username },
+    { label: "GitHub URL", value: "github", placeholder: github },
+    { label: "Image URL", value: "image", placeholder: avatarURL },
+  ];
+
+  const [newUsername, setUsername] = useState<string>(username);
+  const [newGithub, setGithub] = useState<string>(github || "");
+  const [newAvatarURL, setAvatarURL] = useState<string>(avatarURL || "");
+
+  //#region Functions
+  /**
+   * Extracts the new value input by the user and updates the corresponding state.
+   * @param value The new value input by the user
+   * @param field The field of that new value (username, GitHub, or image URL)
+   */
+  const extractValue = (value: string, field: string) => {
+    switch (field) {
+      case "username":
+        setUsername(value);
+        break;
+      case "github":
+        setGithub(value);
+        break;
+      case "image":
+        setAvatarURL(value);
+        break;
+    }
+  };
+
+  const saveEdits = () => {
+    //TODO: Get the values
+    toast.success("Saved successfully", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      pauseOnHover: false,
+      closeOnClick: true,
+      theme: "light",
+      closeButton: false,
+    });
+  };
+  //#endregion
+
+  console.log(newUsername, newGithub, newAvatarURL);
   return (
     <div className="flex flex-col items-center justify-first-line:center gap-y-4">
+      <ToastContainer />
       <div className="relative">
         <div className="w-48 h-48 rounded-full md:w-60 md:h-60 bg-accent-3">
           <img
@@ -35,11 +85,51 @@ const Profile = ({
             alt="Profile Picture"
           />
         </div>
-        <Button
-          className="absolute right-0 top-2"
-          buttonType="icon"
-          icon="src/assets/icons/edit.svg"
-        ></Button>
+        <Popup
+          overlayStyle={{ background: "rgba(0, 0, 0, 0.5)" }}
+          trigger={
+            <Button
+              buttonType="icon"
+              icon="src/assets/icons/edit.svg"
+              className="absolute top-0 right-0 w-12 h-12 p-2 rounded-full"
+              onClick={() => console.log("Change avatar")}
+            ></Button>
+          }
+          modal
+          lockScroll={true}
+        >
+          <div className="flex flex-col py-4 px-6 bg-white rounded-xl gap-y-8 w-[20rem] sm:w-[30rem] md:w-[40rem]">
+            <p className="text-2xl font-semibold text-center text-primary">
+              Edit Profile
+            </p>
+            {editFields.map((field) => (
+              <div className="flex flex-col gap-y-3">
+                <label
+                  className="font-semibold text-secondary-dark"
+                  htmlFor={field.value}
+                >
+                  {field.label}
+                </label>
+                <input
+                  type="text"
+                  name={field.value}
+                  maxLength={field.value === "username" ? 30 : 200}
+                  id={field.value}
+                  defaultValue={field.placeholder}
+                  className="flex-grow px-4 py-4 bg-accent-3 rounded-xl"
+                  onChange={(e) => extractValue(e.target.value, field.value)}
+                />
+              </div>
+            ))}
+            <Button
+              buttonType="text"
+              className="flex items-center justify-center h-12 px-12 m-auto"
+              onClick={saveEdits}
+            >
+              Save
+            </Button>
+          </div>
+        </Popup>
       </div>
 
       <div className="flex flex-col">
