39b41db9e8032e8556a9286e68684543263636b7
harshit-malik7 <harshit4731@gmail.com>
2024-02-20 17:04:43 -0700

    added a post component

---
+++ depresso_espresso/frontend/src/components/data/Post.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Post.module.css';
+
+interface PostProps {
+  imageUrl?: string;
+  content: string;
+}
+
+const Post: React.FC<PostProps> = ({ imageUrl, content }) => {
+  return (
+    <div className={styles.post}>
+      {imageUrl && <img src={imageUrl} alt="Post" className={styles.image} />}
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default Post;
