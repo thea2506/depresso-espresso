272a88fc1243f03c125c54366c7dc61fdda7fe5b
smt12 <59667022+smt12@users.noreply.github.com>
2024-02-24 02:32:25 -0700

    post admin update

--- depresso_espresso/posts/admin.py
+++ depresso_espresso/posts/admin.py
@@ -1,12 +1,37 @@
-from django.contrib import admin
 from .models import Posts
+from django.contrib import admin
+from django import forms
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
 # Register your models here.
+
+class PostCreationForm(forms.ModelForm):
+    """A form for creating new posts."""
+    class Meta:
+        model = Posts
+        fields = ('content', 'image_url', 'publishdate', 'authorid')
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        post = super().save(commit=False)
+
+        if commit:
+            post.save()
+        return post
+
+class PostChangeForm(forms.ModelForm):
+    """A form for updating posts"""
+
+    class Meta:
+        model = Posts
+        fields = ('content', 'image_url', 'publishdate', 'authorid')
 
 
 
 class PostsAdmin(admin.ModelAdmin):
-    fields = ["postid", "authorid", "content", "image_url"]
+    list_display = ('content', 'image_url', 'publishdate', 'authorid', 'postid')
 
 
 admin.site.register(Posts, PostsAdmin)
