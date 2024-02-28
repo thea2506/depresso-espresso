92c2188f58fbd95bb39a7ca6ee64934b1a3ae63e
Thea Nguyen <thea25060@gmail.com>
2024-02-27 22:39:40 -0700

    Hot fixed a bug in profile page

--- depresso_espresso/frontend/src/components/data/PostView.tsx
+++ depresso_espresso/frontend/src/components/data/PostView.tsx
@@ -3,11 +3,10 @@
 import axios from "axios";
 import defaultProfileImage from "../../assets/images/default_profile.jpg";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import { FaHeart, FaComment, FaShare } from "react-icons/fa6";
 import { animated } from "@react-spring/web";
-import { useState } from 'react';
-
+import { useState } from "react";
 
 // components
 import { PostModel } from "./PostModel";
@@ -20,24 +19,22 @@ interface CreatePostViewProps {
 }
 //#endregion
 
-
 /**
  *
  * @param {string} username - The username of the user.
  * @param {string} user_img_url - The URL of the user's avatar
  * @returns
  */
-const PostView = ({post}: CreatePostViewProps) => {
+const PostView = ({ post }: CreatePostViewProps) => {
   const [showComments, setShowComments] = useState(false);
 
   const handleLikeToggle = () => {
     console.log("toggle_like");
 
     axios.post("/toggle_like", { postid: post.postid }).then((response) => {
-      console.log('like response', response.data);
+      console.log("like response", response.data);
       window.location.reload(); // Refresh the page - TODO should be replaced with a more elegant solution in future
     });
-
   };
 
   const handleCommentClick = () => {
@@ -48,61 +45,69 @@ const PostView = ({post}: CreatePostViewProps) => {
   const handleShareClick = () => {
     console.log("Share clicked");
   };
-  
 
   return (
-
     <div className="flex flex-col items-center justify-center w-full px-6 md:px-8 lg:px-0 gap-y-4">
       <ToastContainer />
-  
+
       <animated.form
-        className={'w-full p-8 lg:w-1/2 bg-accent-3 rounded-[1.4rem] flex flex-col gap-y-6 block' }    >
+        className={
+          "w-full p-8 lg:w-1/2 bg-accent-3 rounded-[1.4rem] flex flex-col gap-y-6"
+        }
+      >
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-center gap-x-4">
             <img
               className="object-cover w-12 h-12 rounded-full md:w-13 md:h-13 lg:w-14 lg:h-14"
-              src={post.user_img_url != null ? post.user_img_url : defaultProfileImage}
+              src={
+                post.user_img_url != null
+                  ? post.user_img_url
+                  : defaultProfileImage
+              }
               alt="Profile picture"
             />
             <p className="text-primary">{post.username}</p>
           </div>
 
-          <span>
-            {post.publishdate?.substring(0, 16)}
-          </span>
-       
+          <span>{post.publishdate?.substring(0, 16)}</span>
         </div>
         <p>{post.content}</p>
 
         {/* Image - need to display currently not saved I think*/}
 
-        
-
         {/* { Like, Comment, Share} */}
         <div className="flex items-center justify-between gap-x-4">
           <div className="flex gap-x-4">
             <div className="flex items-center justify-between gap-x-4">
-              <div className="flex gap-x-4 w-full">
+              <div className="flex w-full gap-x-4">
                 <span className="flex items-center gap-x-1">
-                  <FaHeart className="w-6 h-7 text-primary click-icon" onClick={handleLikeToggle} />
+                  <FaHeart
+                    className="w-6 h-7 text-primary click-icon"
+                    onClick={handleLikeToggle}
+                  />
                   <span>{post.likes}</span>
                 </span>
                 <span>
-                  <FaComment className="w-6 h-7 text-primary click-icon" onClick={handleCommentClick} />
+                  <FaComment
+                    className="w-6 h-7 text-primary click-icon"
+                    onClick={handleCommentClick}
+                  />
                   <span>{post.commentcount}</span>
                 </span>
                 <span>
-                  <FaShare className="w-6 h-7 text-primary click-icon" onClick={handleShareClick} />
+                  <FaShare
+                    className="w-6 h-7 text-primary click-icon"
+                    onClick={handleShareClick}
+                  />
                 </span>
               </div>
-            </div>    
+            </div>
           </div>
         </div>
 
         <div className="w-full">
-            {showComments && <CommentList post={post} />} 
+          {showComments && <CommentList post={post} />}
         </div>
-       
 
         {/* Options */}
         {/* <div className="flex items-center justify-between gap-x-4">
