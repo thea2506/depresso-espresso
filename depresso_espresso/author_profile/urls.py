85409f5d6d30c46cae403dd26f14414966df531f
Thea Nguyen <thea25060@gmail.com>
2024-03-05 18:38:19 -0700

    Added frontend render for profile page

--- depresso_espresso/author_profile/urls.py
+++ depresso_espresso/author_profile/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('api/authors/<str:authorid>', views.get_profile, name ='get_profile'),
     path('user_data', views.user_data, name="user_data"),
+    path('api/authors/<str:authorid>', views.get_profile, name ='get_profile'),
+    path('authors/<str:authorid>', views.front_end, name ='frontend'),
     path('user/<str:username>/', views.user_posts, name='user_posts'),
 ]
