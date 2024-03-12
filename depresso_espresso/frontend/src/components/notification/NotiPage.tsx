8012e29ee4b3e972c6c0a485494c0c4b690c92c7
Thea Nguyen <thea25060@gmail.com>
2024-03-11 19:54:59 -0600

    Changed folder inbox to noti

--- depresso_espresso/frontend/src/components/notification/NotiPage.tsx
+++ depresso_espresso/frontend/src/components/notification/NotiPage.tsx
@@ -1,6 +1,6 @@
 import { Notification } from "./Notification";
 
-const InboxPage = () => {
+const NotiPage = () => {
   return (
     <div className="flex flex-col justify-center mx-8 sm:mx-12 lg:mx-[20%] gap-y-8 md:item-center">
       <div className="flex items-center justify-between text-secondary-dark">
@@ -21,4 +21,4 @@ const InboxPage = () => {
   );
 };
 
-export default InboxPage;
+export default NotiPage;
