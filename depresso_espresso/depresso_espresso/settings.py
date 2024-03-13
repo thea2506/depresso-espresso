0b6442ddf93cfde26b1660a3199df52eb0e0132d
Thea Nguyen <thea25060@gmail.com>
2024-03-13 13:58:08 -0600

    Made migrations to db for Heroku

--- depresso_espresso/depresso_espresso/settings.py
+++ depresso_espresso/depresso_espresso/settings.py
@@ -110,9 +110,8 @@ WSGI_APPLICATION = 'depresso_espresso.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-'''   
-commented out to work with heroku-postgres
+ 
+# commented out to work with heroku-postgres
 
 DATABASES = {
     'default': {
@@ -124,14 +123,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
