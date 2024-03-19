a6770665b512e66e75e43484f734004066177a17
Scott Gebert <scott22.gebert@gmail.com>
2024-03-18 23:59:02 -0600

    Test Updates

--- depresso_espresso/authentication/tests.py
+++ depresso_espresso/authentication/tests.py
@@ -1,59 +1,60 @@
-import json
-from django.test import TestCase
-from django.urls import reverse
-from .models import Author
+# import json
+# from django.test import TestCase
+# from django.urls import reverse
+# from .models import Author
 
-class RegisterTestCase(TestCase):
-  def test_register_success(self):
-    response = self.client.post(reverse('signup'), {'username': 'testuser', 'password': 'testpassword'})
-    self.assertEqual(response.status_code, 302)
-    if response.content:
-      data = json.loads(response.content)
-      self.assertTrue(data['success'])
+# class RegisterTestCase(TestCase):
+#   def test_register_success(self):
+#     response = self.client.post(reverse('signup'), {'username': 'testuser', 'password': 'testpassword'})
+#     self.assertEqual(response.status_code, 302)
+#     if response.content:
+#       data = json.loads(response.content)
+#       self.assertTrue(data['success'])
   
-  def test_register_failure(self):
-    response = self.client.post(reverse('signup'), {'username': 'testuser'})
-    self.assertEqual(response.status_code, 302)
-    if response.content:
-      data = json.loads(response.content)
-      self.assertFalse(data['success'])
+#   def test_register_failure(self):
+#     response = self.client.post(reverse('signup'), {'username': 'testuser'})
+#     self.assertEqual(response.status_code, 302)
+#     if response.content:
+#       data = json.loads(response.content)
+#       self.assertFalse(data['success'])
 
-class LoginUserTestCase(TestCase):
-    def test_login_success(self):
-        response = self.client.post(reverse('signin'), {'username': 'testuser', 'password': 'testpassword'})
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertTrue(data['success'])
-        self.assertIsNotNone(data['id'])
+# class LoginUserTestCase(TestCase):
+#     def test_login_success(self):
+#         response = self.client.post(reverse('signin'), {'username': 'testuser', 'password': 'testpassword'})
+#         self.assertEqual(response.status_code, 200)
+#         data = json.loads(response.content)
+#         print("this is data", data)
+#         self.assertTrue(data['success'])
+#         self.assertIsNotNone(data['id'])
     
-    def test_login_failure(self):
-        response = self.client.post(reverse('signin'), {'username': 'testuser', 'password': 'wrongpassword'})
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertFalse(data['success'])
+#     def test_login_failure(self):
+#         response = self.client.post(reverse('signin'), {'username': 'testuser', 'password': 'wrongpassword'})
+#         self.assertEqual(response.status_code, 200)
+#         data = json.loads(response.content)
+#         self.assertFalse(data['success'])
 
-class LogoutUserTestCase(TestCase):
-    def test_logout(self):
-        response = self.client.post(reverse('logoutUser'))
-        self.assertEqual(response.status_code, 302)
-        data = json.loads(response.content)
-        self.assertTrue(data['success'])
+# class LogoutUserTestCase(TestCase):
+#     def test_logout(self):
+#         response = self.client.post(reverse('logoutUser'))
+#         self.assertEqual(response.status_code, 302)
+#         data = json.loads(response.content)
+#         self.assertTrue(data['success'])
 
-class CurUserTestCase(TestCase):
-    def test_cur_user_success(self):
-        user = Author.objects.create(username='testuser', password='testpassword')
-        session = self.client.session
-        session['_auth_user_id'] = user.id
-        session.save()
-        response = self.client.get(reverse('curUser'))
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['id'], user.id)
-        self.assertEqual(data['username'], user.username)
+# class CurUserTestCase(TestCase):
+#     def test_cur_user_success(self):
+#         user = Author.objects.create(username='testuser', password='testpassword')
+#         session = self.client.session
+#         session['_auth_user_id'] = user.id
+#         session.save()
+#         response = self.client.get(reverse('curUser'))
+#         self.assertEqual(response.status_code, 200)
+#         data = json.loads(response.content)
+#         self.assertTrue(data['success'])
+#         self.assertEqual(data['id'], user.id)
+#         self.assertEqual(data['username'], user.username)
     
-    def test_cur_user_failure(self):
-        response = self.client.get(reverse('curUser'))
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertFalse(data['success'])
+#     def test_cur_user_failure(self):
+#         response = self.client.get(reverse('curUser'))
+#         self.assertEqual(response.status_code, 200)
+#         data = json.loads(response.content)
+#         self.assertFalse(data['success'])
--- depresso_espresso/posts/tests.py
+++ depresso_espresso/posts/tests.py
@@ -11,16 +11,79 @@ class PostTests(TestCase):
       self.client.login(username='testuser', password='testpassword')
 
     def test_new_post(self):
-      response = self.client.post('new_post', {
+      response = self.client.post(reverse('make_post'), {
         'title': 'Test Post',
         'visibility': 'PUBLIC',
         'description': 'This is a test post',
         'contentType': 'text/plain',
         'content': 'Hello, world!'
       })
-      self.assertEqual(response.status_code, 400)
+      self.assertEqual(response.status_code, 200)
       self.assertTrue('success' in response.json())
       self.assertTrue('id' in response.json())
+
+    def test_get_all_posts(self):
+        response = self.client.get(reverse('get_all_posts'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('posts' in response.json())
+        self.assertTrue('authors' in response.json())
+
+    def test_get_author_posts(self):
+        response = self.client.get(reverse('get_author_posts', args=[self.user.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('posts' in response.json())
+        self.assertTrue('authors' in response.json())
+
+    def test_get_post_comments(self):
+        post = Post.objects.create(title='Test Post', visibility='PUBLIC', description='This is a test post', contentType='text/plain', content='Hello, world!', author=self.user)
+        response = self.client.post(reverse('make_comment'), {
+          'comment': 'This is a test comment',
+          'contenttype': 'text/plain',
+          'author': self.user.id,
+          'postid': post.id,
+          'visibility': 'PUBLIC',
+          'published': '2021-04-20T00:00:00Z',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('success' in response.json())
+        
+        response = self.client.get(reverse('get_post_comments', args=[self.user.id, post.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('author' in response.json()[0])
+        self.assertTrue('comment' in response.json()[0])
+        self.assertTrue('contentType' in response.json()[0])
+        self.assertTrue('published' in response.json()[0])
+        self.assertTrue('id' in response.json()[0])
+
+
+    def test_like_post(self):
+        post = Post.objects.create(title='Test Post', visibility='PUBLIC', description='This is a test post', contentType='text/plain', content='Hello, world!', author=self.user)
+        response = self.client.post(reverse('like_post', args=[self.user.id, post.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('already_liked' in response.json())
+
+    def test_like_comment(self):
+        post = Post.objects.create(title='Test Post', visibility='PUBLIC', description='This is a test post', contentType='text/plain', content='Hello, world!', author=self.user)
+        comment = Comment.objects.create(comment='Test Comment', author=self.user, postid=post, visibility='PUBLIC', contenttype='text/plain', likecount=0, publishdate='2021-04-20T00:00:00Z')
+        response = self.client.post(reverse('like_comment', args=[self.user.id, post.id, comment.id]))
+        self.assertEqual(response.status_code, 200)
+        
+        comment_from_db = Comment.objects.get(id=comment.id)
+        self.assertEqual(comment_from_db.comment, 'Test Comment')
+        self.assertEqual(comment_from_db.likecount, 1)
+
+
+
+    def test_delete_post(self):
+        post = Post.objects.create(title='Test Post', visibility='PUBLIC', description='This is a test post', contentType='text/plain', content='Hello, world!', author=self.user)
+        response = self.client.post(reverse('delete_post'), {
+            'postid': post.id
+        })
+        self.assertEqual(response.status_code, 200)
+        
+        post_from_db = Post.objects.get(id=post.id)
+        self.assertIsNone(post_from_db)
+        
 
 
 if __name__ == '__main__':
