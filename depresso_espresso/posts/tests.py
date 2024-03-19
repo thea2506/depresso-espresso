d8481bcebae47e5df91632ba9b504d5590f07b7a
Scott Gebert <scott22.gebert@gmail.com>
2024-03-18 23:37:21 -0600

    Post test

--- depresso_espresso/posts/tests.py
+++ depresso_espresso/posts/tests.py
@@ -1,3 +1,27 @@
+import unittest
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post, Comment, Author
+
+class PostTests(TestCase):
+    def setUp(self):
+      self.user = Author.objects.create_user(username='testuser', password='testpassword')
+      self.client.login(username='testuser', password='testpassword')
+
+    def test_new_post(self):
+      response = self.client.post('new_post', {
+        'title': 'Test Post',
+        'visibility': 'PUBLIC',
+        'description': 'This is a test post',
+        'contentType': 'text/plain',
+        'content': 'Hello, world!'
+      })
+      self.assertEqual(response.status_code, 400)
+      self.assertTrue('success' in response.json())
+      self.assertTrue('id' in response.json())
+
+
+if __name__ == '__main__':
+    unittest.main()
