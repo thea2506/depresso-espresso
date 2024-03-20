6ba51286d5b7172bd3efd8a85bfd98b021257b68
Scott Gebert <scott22.gebert@gmail.com>
2024-03-20 13:05:11 -0600

    Curr user test updates

--- depresso_espresso/authentication/tests.py
+++ depresso_espresso/authentication/tests.py
@@ -1,8 +1,12 @@
 import json
+import uuid
 from django.http import JsonResponse
 from django.test import TestCase
 from django.urls import reverse
 from .models import Author
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 class RegisterTestCase(TestCase):
   def test_register_success(self):
@@ -45,3 +49,41 @@ class LogoutUserTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
         self.assertTrue(data['success'])
+        
+        
+class CurUserTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.author = Author.objects.create(
+          id=uuid.uuid4(),
+          type="author",
+          url="http://localhost:8000/author/1",
+          host="http://localhost:8000/",
+          displayName="Author 1",
+          username="author1",
+          github="author1",
+          profileImage="http://localhost:8000/media/profile.jpg"
+        )
+        
+        self.client.force_login(user=self.author)
+
+    def test_curUser_with_valid_session(self):
+        response = self.client.get(reverse('curUser'), session=self.client.session)
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {
+            'type': self.author.type,
+            'id': str(self.author.id),
+            'username': self.author.username,
+            'displayName': self.author.displayName,
+            'host': self.author.host,
+            'url': self.author.url,
+            'github': self.author.github,
+            'profileImage': self.author.profileImage,
+            'success': True
+        })
+
+    def test_curUser_with_invalid_session(self):
+        self.client.logout()
+        response = self.client.get(reverse('curUser'))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'success': False})
