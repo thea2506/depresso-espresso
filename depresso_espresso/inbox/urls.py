c9f0be16276418840e0850509369aed9649791c6
Thea Nguyen <thea25060@gmail.com>
2024-03-20 09:22:00 -0600

    Removed unused route

--- depresso_espresso/inbox/urls.py
+++ depresso_espresso/inbox/urls.py
@@ -2,11 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('authors/<str:authorid>/espresso-api/inbox',
-         views.handle_inbox, name="handle_inbox"),
-    path('create_notification', views.create_notification, name="signup"),
-    path('get_notifications/<str:authorid>',
-         views.get_notifications, name="get_notifications"),
 
     # 1. GET/POST //service/authors/{AUTHOR_ID}/inbox/
     path('espresso-api/authors/<str:authorid>/inbox',
