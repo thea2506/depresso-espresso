ece4ac526248794d598012786117e8e45cc954b9
Afra Rahmanfard <afra.rahmanfard@gmail.com>
2024-03-20 00:25:28 -0600

    Fixing node to node

--- depresso_espresso/author_profile/views.py
+++ depresso_espresso/author_profile/views.py
@@ -133,7 +133,7 @@ def get_authors(request):
                 res = client.getresponse()
                 authorsBytes = res.read()
 
-                author_data = authorsBytes.json()
+                author_data = json.loads(authorsBytes)
                 print(author_data)
                 for author in author_data["items"]:
                     # Check if the author already exists in our db
