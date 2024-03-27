ee750a3e5f3084997f5d64dd82850019d935f578
Thea Nguyen <thea25060@gmail.com>
2024-03-26 20:00:47 -0600

    For heroku DB

--- depresso_espresso/depresso_espresso/settings.py
+++ depresso_espresso/depresso_espresso/settings.py
@@ -122,26 +122,15 @@ WSGI_APPLICATION = 'depresso_espresso.wsgi.application'
 
 # commented out to work with heroku-postgres
 
+DATABASES = {'default': dj_database_url.config()}
+
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'SocialDistribution',
-#         'USER': 'postgres',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-# DATABASES = {'default': dj_database_url.config()}
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
