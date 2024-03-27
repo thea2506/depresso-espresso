e59edb224ca6c8143ae8532a7b2d85b8021e2a8b
Thea Nguyen <thea25060@gmail.com>
2024-03-27 15:50:55 -0600

    Fixed shared display ui error

--- depresso_espresso/depresso_espresso/settings.py
+++ depresso_espresso/depresso_espresso/settings.py
@@ -179,7 +179,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist/static')
+    os.path.join(BASE_DIR, 'frontend/dist/static'),
+    os.path.join(BASE_DIR, 'frontend/dist'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
