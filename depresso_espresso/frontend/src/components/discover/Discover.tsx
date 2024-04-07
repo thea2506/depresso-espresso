3e2f430aaa62a90d47e433651aa5a61cf0417076
Thea Nguyen <thea25060@gmail.com>
2024-04-06 20:53:07 -0600

    Fixed checkSameOrigin in Discover.tsx

--- depresso_espresso/frontend/src/components/discover/Discover.tsx
+++ depresso_espresso/frontend/src/components/discover/Discover.tsx
@@ -21,7 +21,7 @@ const checkSameOrigin = (url: string) => {
     if (url.includes(windowOrigin)) return true;
   }
 
-  return false;
+  return url.includes(windowOrigin);
 };
 
 const Discover = () => {
@@ -50,8 +50,7 @@ const Discover = () => {
       <div className="flex flex-col w-4/5 gap-4">
         {loading ? (
           <div className="m-8 text-lg font-semibold text-primary opacity-70">
-            Loading... or there are no other servers connected to us
-            yet
+            Loading... or there are no other servers connected to us yet
           </div>
         ) : (
           allAuthors.map((author, key: number) => {
