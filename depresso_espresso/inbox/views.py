a1139ffe9c88d6b0e0a55102e6c765f8db929c28
Thea Nguyen <thea25060@gmail.com>
2024-04-09 00:44:56 -0600

    debug Heroku

--- depresso_espresso/inbox/views.py
+++ depresso_espresso/inbox/views.py
@@ -441,7 +441,8 @@ def handle_post(request, author_id):
         print(actor_obj)
         if not Post.objects.filter(id=uuid.UUID(id)).exists():
             print(">>>DATA", data)
-            data["author"] = AuthorSerializer(instance=actor_obj).data
+            data["author"] = AuthorSerializer(
+                instance=actor_obj, context={"request": request}).data
             serializer.save(id=uuid.UUID(id))
 
         notification_object = Notification.objects.get_or_create(author=author_object)[
